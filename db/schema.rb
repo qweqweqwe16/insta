02c67a887ccd224e199f849d89f93e384db7d1e1
qweqweqwe16 <drogbazuka@mail.ru>
2018-12-28 03:43:34 +0300

    add pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_27_232738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "login"
     t.string "password"
